7cfc473bfa936ff5199d55d1f63303f0ff5e4835
OBOLLL20333 <97986329+OBOLLL20333@users.noreply.github.com>
2023-01-17 04:08:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,9 +20,15 @@ $(function () {
 		arrows: false,
 	});
 
+	$('.article-slider__box').slick({
+		prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="images/arrow-left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="images/arrow-right.svg" alt=""></button>',
+	});
+
 	var mixer = mixitup('.gallery__inner', {
 		load: {
 			filter: '.living'
 		}
 	});
 })
+
